09b3922bba7b757e610b88ca64c323e71045e4cd
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-07-16 14:43:02 +0530

    Login php done

--- users.php
+++ users.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Realtime Chat App</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php require_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php
+
+                ?>
                 <div class="content">
                     <img src="1.jpg" alt="">
                     <div class="details">
